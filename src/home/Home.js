6d49f34aee2a4f23371fec2f5aa35e398db8a9d1
kabilangr <g.r.kabilan@gmail.com>
2023-06-25 12:06:42 +0530

    home page changes

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component, useEffect, useState } from 'react'
 import Navigation from '../navigation/Navigation';
 import { Home as HomePage }  from '../utils/Constants';
+import topIcon from "../assets/scroll-to-top.png"
 import './Home.css';
 
 
 const Home = () => {
     const [showButton, setShowButton] = useState(false)
+    const [showScrollBar,setShowScrollBar] = useState(false)
     useEffect(() => {
         const handleScrollButtonVisiblity = () => {
             window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
@@ -23,14 +25,14 @@ const Home = () => {
             <div className='top-nav'><Navigation currentPage={HomePage}/></div>
             <div className="homePage-container">
                 <section className='top-Section'>
-                    <article className='article'>
-                        GoodLuck is made by strong Will 
-                    </article>
+                    <article className='article typed'>Hi, My name is kabilan.</article>
+                    <div className='top-animation'>
+                        <div className='top-animation-circle'></div>
+                        <div className='top-animation-rectangle'></div>
+                    </div>
                 </section>
                 <section className='top-Section'>
-                    <article className='article'>
-                        GoodLuck is made by strong Will 
-                    </article>
+                    <article className='article'>I am a Full-stack Developer.</article>
                 </section>
                 <section className='top-Section'>
                     <article className='article'>
@@ -38,13 +40,14 @@ const Home = () => {
                     </article>
                 </section>
             </div>
-            {
-                showButton && ( <div className = {'scrollToTop '}>          
-                                <button className = "fixed bottom-5 right-7 z-50 cursor-pointer p-4"
-                            onClick = {handleScrollToTop} >
-                            < img src = "/icons/new-up-arrow.png" alt = "scrollToTop" />
-
-                            </button> </div > )} </div>
+            {showButton && (
+                <div className='scrollToTopContainer'>          
+                    <button className="scrollToTopButton" onClick={handleScrollToTop} >
+                        <img className='scroll-to-top-icon' src={topIcon} alt="scrollToTop" />
+                    </button>
+                </div > 
+            )} 
+        </div>
     )
 }
 
